8e72b52ff47308675af82f350a885fd29e7f3348
kaneargentaro <kane_argentaro@hotmail.com>
2025-03-31 19:41:18 +1100

    feat(temporal-201): durable execution exercise

--- temporal-course/temporal-201/exercises/durable-execution/practice/src/activities.ts
+++ temporal-course/temporal-201/exercises/durable-execution/practice/src/activities.ts
@@ -1,15 +1,15 @@
 import axios from 'axios';
-
-// TODO import everything from the @temporalio/activity package as activity
+import * as activity from '@temporalio/activity'
 
 import { TranslationActivityInput, TranslationActivityOutput } from './shared';
 
 export async function translateTerm(input: TranslationActivityInput): Promise<TranslationActivityOutput> {
-  // TODO Define an Activity logger
+  const context = activity.Context.current();
 
-  // TODO log Activity invocation, at the Info level, and include the term being
-  //      translated and the language code as name-value pairs
-
+  context.log.info(`Running translate term`, {
+    term: input.term,
+    langaugeCode: input.languageCode
+  });
   const lang = encodeURIComponent(input.languageCode);
   const term = encodeURIComponent(input.term);
 
@@ -18,22 +18,24 @@ export async function translateTerm(input: TranslationActivityInput): Promise<Tr
   try {
     const response = await axios.get(url);
     const content = response.data;
-    // TODO  use the Debug log level to log the successful translation and include the
-    //       translated term as a name-value pair
+    context.log.debug(`Successfully translated`, {
+      content: content
+    });
     return { translation: content };
   } catch (error: any) {
     if (error.response) {
-      // TODO  use the Error log level to log the failed response and include the
-      //       response status and response data using error.response.status
-      //       and error.response.data
+      context.log.error(`HTTP Error`, {
+        status: error.response.status,
+        data: error.response.data
+      });
       throw new Error(`HTTP Error ${error.response.status}: ${error.response.data}`);
     } else if (error.request) {
-      // TODO  use the Error log level to log the failed request and include the
-      //       value of error.request
+      context.log.error(`Request Error`, {
+        request: error.request
+      });
       throw new Error(`Request error:  ${error.request}`);
     }
-    // TODO  use the Error log level to log that something else went wrong with
-    //       the translation. Print out the value of `error`.
+    context.log.error(`Something else failed during translation.`, error);
     throw new Error('Something else failed during translation.');
   }
 }
--- temporal-course/temporal-201/exercises/durable-execution/practice/src/workflows.ts
+++ temporal-course/temporal-201/exercises/durable-execution/practice/src/workflows.ts
@@ -1,5 +1,4 @@
-// TODO import the sleep and log packages here by adding it to the list.
-import { proxyActivities } from '@temporalio/workflow';
+import { proxyActivities, log, sleep } from '@temporalio/workflow';
 // Only import the activity types
 import type * as activities from './activities';
 import { TranslationWorkflowInput, TranslationWorkflowOutput } from './shared';
@@ -9,28 +8,26 @@ const { translateTerm } = proxyActivities<typeof activities>({
 });
 
 export async function sayHelloGoodbyeWorkflow(input: TranslationWorkflowInput): Promise<TranslationWorkflowOutput> {
-  // TODO Log, at the Info level, when the Workflow function is invoked
-  //      and be sure to include the name passed as input
+  log.info(`The workflow function has been evoked`, { "name": input.name })
 
   const helloInput = {
     term: 'Hello',
     languageCode: input.languageCode,
   };
 
-  // TODO Log, at the Debug level, a message about the Activity to be executed,
-  //      be sure to include the language code passed as input
+  log.debug(`Executing translateTerm for hello`, {"languageCode": input.languageCode})
   const helloResult = await translateTerm(helloInput);
   const helloMessage = `${helloResult.translation}, ${input.name}`;
 
-  // TODO: (Part C): log a message at the Debug level and then start a Timer for 10 seconds
+  log.debug(`Sleeping between translation calls`)
+  await sleep("10 seconds");
 
   const goodbyeInput = {
     term: 'Goodbye',
     languageCode: input.languageCode,
   };
 
-  // TODO Log, at the Debug level, a message about the Activity to be executed,
-  //      be sure to include the language code passed as input
+  log.debug(`Executing translateTerm for goodbye`, {"languageCode": input.languageCode})
   const goodbyeResult = await translateTerm(goodbyeInput);
   const goodbyeMessage = `${goodbyeResult.translation}, ${input.name}`;
 
