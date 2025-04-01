1631e4bca5d9579eb8acb6ee6b3f172acd3ea6d2
kaneargentaro <kane_argentaro@hotmail.com>
2025-04-01 15:34:24 +1100

    feat(temporal-201): testing exercise

--- temporal-course/temporal-201/exercises/testing-code/practice/src/mocha/workflows.test.ts
+++ temporal-course/temporal-201/exercises/testing-code/practice/src/mocha/workflows.test.ts
@@ -1,52 +1,44 @@
-/*
 import { TestWorkflowEnvironment } from '@temporalio/testing';
 import { before, describe, it } from 'mocha';
 import { Worker } from '@temporalio/worker';
-import {sayHelloGoodbyeWorkflow } from '../workflows';
+import { sayHelloGoodbyeWorkflow } from '../workflows';
 import * as activities from '../activities';
 import assert from 'assert';
 
 describe('SayHelloGoodbye workflow', () => {
-  let testEnv: TestWorkflowEnvironment;
+    let testEnv: TestWorkflowEnvironment;
 
-  before(async() => {
-    testEnv = await TestWorkflowEnvironment.createTimeSkipping();
-  });
-
-  after(async () => {
-    await testEnv?.teardown();
-  });
-
-  it('successfully completes French translation', async () => {
-
-    const { client, nativeConnection } = testEnv;
-
-    const workflowInput = {
-      name:         "Pierre",
-      languageCode: "fr",
-    };
-
-    const worker = await Worker.create({
-      connection: nativeConnection,
-      taskQueue: 'test',
-      workflowsPath: require.resolve('../workflows'),
-      activities,
+    before(async () => {
+        testEnv = await TestWorkflowEnvironment.createTimeSkipping();
     });
 
-    const result = await worker.runUntil(
-      client.workflow.execute(sayHelloGoodbyeWorkflow, {
-        args: [workflowInput],
-        workflowId: 'test',
-        taskQueue: 'test',
-      })
-    );
+    after(async () => {
+        await testEnv?.teardown();
+    });
 
-    // TODO: Assert that the HelloMessage field in the
-    //       result is: Bonjour, Pierre
+    it('successfully completes French translation', async () => {
+        const { client, nativeConnection } = testEnv;
 
-    // TODO: Assert that the GoodbyeMessage field in the
-    //       result is: Au revoir, Pierre
+        const workflowInput = {
+            name: 'Pierre',
+            languageCode: 'fr',
+        };
 
-  });
+        const worker = await Worker.create({
+            connection: nativeConnection,
+            taskQueue: 'test',
+            workflowsPath: require.resolve('../workflows'),
+            activities,
+        });
+
+        const result = await worker.runUntil(
+            client.workflow.execute(sayHelloGoodbyeWorkflow, {
+                args: [workflowInput],
+                workflowId: 'test',
+                taskQueue: 'test',
+            })
+        );
+        assert.equal(result.helloMessage, 'Bonjour, Pierre');
+        assert.equal(result.goodbyeMessage, 'Au revoir, Pierre');
+    });
 });
-*/
